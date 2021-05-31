1f8c311ae93542caa7a960cff6cd85d6fb428a3b
Ward V <85070627+WardVx@users.noreply.github.com>
2021-05-31 18:29:48 +0200

    Update Piston.py

--- Piston.py
+++ Piston.py
@@ -1,32 +1,31 @@
 import RPi.GPIO as GPIO
 import time
-cur_X = 0 #initial value of servo motor
+
 def setup():
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(11,GPIO.OUT)
-        global servo
-        servo=GPIO.PWM(11,50)
-        servo.start(2.5)
-        servo.ChangeDutyCycle(2.5)
-        #start pwm with Duty Cycle is 2% --> Pluse with = 2%*20ms = 0.4ms
-        #Create PWM on pin 11 with frequency 50Hz --> period 20ms
-def ServoUp():
-        global cur_X
-        cur_X = 10
-        if cur_X <10:
-                cur_X = 10
-        servo.ChangeDutyCycle(cur_X)
-        #servo.ChangeDutyCycle(0)
-        time.sleep(1)
-def ServoDown():
-        global cur_X
-        cur_X = -10
-        if cur_X <-10:
-                cur_X = -10
-        servo.ChangeDutyCycle(cur_X)
-        #servo.ChangeDutyCycle(10)
-        time.sleep(1)
-def close():
-        servo.stop()
+        global PistonU
+        global PistonD
+        
+        PistonU=GPIO.PWM(11,50)
+        #Piston omhoog staat hier als poort 11
+        
+        PistonD=GPIO.PWM(11,50)
+        #Piston omlaag staat hier als poort 11
+
+def PistonUp():
+        PistonU.ChangeDutyCycle(10)
+        time.sleep(2)
+        #Piston gaat gedurende 2 seconden omhoog
+        PistonU.ChangeDutyCycle(0)
+        
+def PistonDown():
+        PistonD.ChangeDutyCycle(10)
+        time.sleep(2)
+        #Piston gaat gedurende 2 seconden omlaag
+        PistonD.ChangeDutyCycle(0)
+        
+def close(): 
+         piston.stop()
 if __name__ == '__main__':
         setup()
