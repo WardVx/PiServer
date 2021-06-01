268f189cc5b530bd5d1c650d93999234471de99c
Ward V <85070627+WardVx@users.noreply.github.com>
2021-06-01 22:43:28 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -49,8 +49,8 @@ while True:
                         data = tcpCliSock.recv(BUFSIZE)
                         
                         if not data:
-                                sys.stdout.write(CURSOR_UP_ONE)
-                                sys.stdout.write(ERASE_LINE)                              
+                                #sys.stdout.write(CURSOR_UP_ONE)
+                                #sys.stdout.write(ERASE_LINE)                              
                                 break
                                 
                         if data == ctrCmd[0]:
