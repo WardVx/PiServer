c8c51172bae49c0c5e337ae012b1e625b5c68aa9
Ward V <85070627+WardVx@users.noreply.github.com>
2021-06-01 16:40:49 +0200

    Create Piston2.py

---
+++ Piston2.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(23,GPIO.LOW)
+  GPIO.setup(24,GPIO.LOW)
+
+def PistonUp():
+  GPIO.output(23, GPIO.HIGH)
+  time.sleep(2)
+  GPIO.output(23, GPIO.LOW)
+
+def PistonDown():
+  GPIO.output(24, GPIO.HIGH)
+  time.sleep(2)
+  GPIO.output(24, GPIO.LOW)
+  
+def close():
+  piston.stop
+if __name__ == '__main__':
+  setup()
