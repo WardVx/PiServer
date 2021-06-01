37a51215a91c43d3bd14299766987a134ff5cf19
Ward V <85070627+WardVx@users.noreply.github.com>
2021-06-01 15:56:19 +0200

    Update Piston.py

--- Piston.py
+++ Piston.py
@@ -9,9 +9,11 @@ def setup():
         
         PistonU=GPIO.PWM(11,50)
         #Piston omhoog staat hier als poort 11
+        #Poort 9 is GROUND
         
-        PistonD=GPIO.PWM(11,50)
-        #Piston omlaag staat hier als poort 11
+        PistonD=GPIO.PWM(12,50)
+        #Piston omlaag staat hier als poort 12
+        #Poort 9 is GROUND
 
 def PistonUp():
         PistonU.ChangeDutyCycle(10)
