2003e409c6537a8b0182d46a34544c0731e18324
Ward V <85070627+WardVx@users.noreply.github.com>
2021-07-15 01:11:53 +0200

    commit

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
-
 class Server_Settings
-Console_LOG = False
-Port = 21567
+    Console_LOG = False
+    # Zorgt ervoor dat je kan zien wat de server doet in de console
+     Port = 21567
 
 class Piston_Settings
-Pin_Omhoog = 23
-Pin_Omlaag = 24
-Piston_Reistijd = 2
+    Pin_Omhoog = 23
+    Pin_Omlaag = 24
+    # https://www.raspberrypi.org/documentation/usage/gpio/
+    Piston_Reistijd = 2
+    # Tijd in seconden
